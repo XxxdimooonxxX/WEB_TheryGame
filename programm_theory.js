1b39350a7657f3895fe5bd967e4f10e099b48a52
Dmitry <dima2017.koshelev@yandex.ru>
2020-12-06 22:12:07 +0300

    add size array arrSA, x-size and y-size, in JS, and delete before table in HTML. Add in HTML, input size table. Add in CSS, style for input size table.

--- programm_theory.js
+++ programm_theory.js
@@ -5,9 +5,6 @@
 //																						//
 //		@XxxdimooonxxX 2020																//
 //////////////////////////////////////////////////////////////////////////////////////////
-////////////////////// Input size matrix: ////////////////////////////////////////////////
-var size_matrix	= Number(prompt("aa"));	//size matrix									//
-//////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////// Global variable: ///////////////////////////////////////////////
 var arrSA;		//Main Matrix															//
 //======================================================================================//
@@ -19,76 +16,97 @@ var va	= 0,	//va	- I																//
 	la	= 0;	//la	- V																//
 //======================================================================================//
 //array, which will help for search strategiya:											//
-var arrVA	= new Array(size_matrix),													//
-	arrMA	= new Array(size_matrix),													//
-	arrSE	= new Array(size_matrix),													//
-	arrGUR	= new Array(size_matrix),													//
-	arrLA	= new Array(size_matrix);													//
+var arrVA, arrMA, arrSE, arrGUR, arrLA;													//
 //////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////// Programm: //////////////////////////////////////////////////////
-//======================================================================================//
-/////////////////////// Create matrix ////////////////////////////////////////////////////
-arrSA = new Array(size_matrix);	//array "main matrix"
+/////////////////////// Create matrix: ///////////////////////////////////////////////////
+var len_x = document.querySelector("input#size_x");
+var len_y = document.querySelector("input#size_y");
+var l_x = 0, l_y = 0;	//size matrix, x - state nature, y - "strategii"
+var bt_create = document.querySelector("div#create");
+var ass = 0;
+bt_create.onclick = () =>{
+	if(ass != 0){
+		var t_matrix = document.querySelector(".table_dark");
+		var len_tr_del = t_matrix.childElementCount;
 
-//create array in array "main matrix"
-for(i = 0; i < size_matrix; i++){
-	arrSA[i] = new Array(size_matrix);
-}
+		for(i = 0; i < len_tr_del; i++){
+			var tr_del = t_matrix.firstElementChild;
+			t_matrix.removeChild(tr_del);
+		}
+	}
+	ass++;
 
-//create table-matrix(main matrix) in HTML:
-var main_matrix	= document.querySelector(".table_dark");	//get element .table_dark
-var A_size		= 1;										//счетчик стратегий, для таблицы
+	l_x = Number(len_x.value);
+	l_y = Number(len_y.value);
 
-for(i = 0; i < size_matrix + 1; i++){
-	//Для строки таблицы-матрицы, в которой указываются состояние природы
-	if(i == 0){
-		var tr_matrix	= document.createElement("tr");//create tr
-		for(j = 0; j < size_matrix + 1; j++){
-			var th_matrix	= document.createElement("th");//create th
-			
-			if(j == 0){
-				th_matrix.innerHTML = "S/A";
-			}else{
-				th_matrix.innerHTML = "B" + j;
+	arrSA	= new Array(l_y);	//array "main matrix"
+	arrVA	= new Array(l_y);
+	arrMA	= new Array(l_y);
+	arrSE	= new Array(l_y);
+	arrGUR	= new Array(l_y);
+	arrLA	= new Array(l_y);
+
+	//create array in array "main matrix"
+	for(i = 0; i < l_y; i++){
+		arrSA[i] = new Array(l_x);
+	}
+
+	//create table-matrix(main matrix) in HTML:
+	var main_matrix = document.querySelector(".table_dark"); //get element .table_dark
+	var A_size = 1; //счетчик стратегий, для таблицы
+
+	for(i = 0; i < l_y + 1; i++){
+		//Для строки таблицы-матрицы, в которой указываются состояние природы
+		if(i == 0){
+			var tr_matrix = document.createElement("tr");//create tr
+			for(j = 0; j < l_x + 1; j++){
+				var th_matrix = document.createElement("th");//create th
+				
+				if(j == 0){
+					th_matrix.innerHTML = "S/A";
+				}else{
+					th_matrix.innerHTML = "B" + j;
+				}
+
+				tr_matrix.appendChild(th_matrix);
 			}
-
+			main_matrix.appendChild(tr_matrix);
+		}
+		//для строк-стратегий, указывается номер стратегии и вставляет поля-ввода
+		if(i != 0){
+			var tr_matrix = document.createElement("tr");//create tr
+			var th_matrix = document.createElement("th");//create th
+			
+			th_matrix.innerHTML = "A" + A_size;
+			A_size++;
 			tr_matrix.appendChild(th_matrix);
+
+			for(j = 1; j < l_x + 1; j++){
+				var td_matrix = document.createElement("td");//create td
+				var inps = document.createElement("input");
+				inps.name = "matrix";
+				inps.id = "matrix";
+				inps.type = "number";
+
+				td_matrix.appendChild(inps);
+				tr_matrix.appendChild(td_matrix);
+			}
+			main_matrix.appendChild(tr_matrix);
 		}
-		main_matrix.appendChild(tr_matrix);
 	}
-	//для строк-стратегий, указывается номер стратегии и вставляет поля-ввода
-	if(i != 0){
-		var tr_matrix	= document.createElement("tr");//create tr
-
-		var th_matrix	= document.createElement("th");//create th
-		th_matrix.innerHTML = "A" + A_size;
-		A_size++;
-		tr_matrix.appendChild(th_matrix);
-
-		for(j = 1; j < size_matrix + 1; j++){
-			var td_matrix	= document.createElement("td");//create td
-			var inps		= document.createElement("input");
-			inps.name = "matrix";
-			inps.type = "number";
-
-			td_matrix.appendChild(inps);
-			tr_matrix.appendChild(td_matrix);
-		}
-		main_matrix.appendChild(tr_matrix);
-	}
+	//END create table-matrix(main matrix) in HTML
 }
-//END create table-matrix(main matrix) in HTML
-/////////////////////// END Create matrix ////////////////////////////////////////////////
-//======================================================================================//
-////////////// Part programm, which search number "strategii" ////////////////////////////
-var bt_result = document.querySelector("div.result");	//button result, which performed main code
+//////////////////////////////////////////////////////////////////////////////////////////
+////////////// PART PROGRAMM, WHICH SEARCH NUMBER "strategii" ////////////////////////////
+var bt_result = document.querySelector("div#result");	//button result, which performed main code
 bt_result.onclick = () =>{
 	//====== Read data from matrix-table =================
 	//matrix - name inputs when use matrix-table:
-	var inputs_data	= document.querySelectorAll("input");
-	var id			= 0;	//id for array inputes_data:
-	for(i = 0; i < size_matrix; i++){
-		for(j = 0; j < size_matrix; j++){
+	var inputs_data = document.querySelectorAll("input#matrix");
+	var id = 0; //id for array inputes_data:
+	for(i = 0; i < l_y; i++){
+		for(j = 0; j < l_x; j++){
 			arrSA[i][j] = inputs_data[id].value;
 			id++;
 		}
@@ -100,10 +118,10 @@ bt_result.onclick = () =>{
 	//================================== I =========================================
 	var a;	//var. for trash
 	//zicl for search min in all strtegii Samsung:
-	for (i = 0; i < size_matrix; i++) {
+	for (i = 0; i < l_y; i++) {
 		a = arrSA[i][0];
 		arrVA[i] = a;
-		for (j = 1; j < size_matrix; j++) {
+		for (j = 1; j < l_x; j++) {
 			if (arrSA[i][j] < a) {
 				arrVA[i] = a;
 			}
@@ -111,7 +129,7 @@ bt_result.onclick = () =>{
 	}
 	//Search maxed in arrVA
 	var va1 = arrVA[0];	//var. for search maxed in arrVA
-	for (i = 1; i < size_matrix; i++) {
+	for (i = 1; i < l_y; i++) {
 		if (va1 < arrVA[i]) {
 			va1 = arrVA[i];
 			va = i;
@@ -122,10 +140,10 @@ bt_result.onclick = () =>{
 	//================================== END - I ===================================
 	//================================== II ========================================
 	//zicl for search max in all strtegii Samsung:
-	for (i = 0; i < size_matrix; i++) {
+	for (i = 0; i < l_y; i++) {
 		a = arrSA[i][0];
 		arrMA[i] = a;
-		for (j = 1; j < size_matrix; j++) {
+		for (j = 1; j < l_x; j++) {
 			if (arrSA[i][j] > a) {
 				a = arrSA[i][j];
 				arrMA[i] = a;
@@ -134,7 +152,7 @@ bt_result.onclick = () =>{
 	}
 	//Search maxed in arrMA
 	var ma1 = arrMA[0];	//var. for trash
-	for (i = 1; i < size_matrix; i++) {
+	for (i = 1; i < l_y; i++) {
 		if (ma1 < arrMA[i]) {
 			ma1 = arrMA[i];
 			ma = i;
@@ -146,12 +164,12 @@ bt_result.onclick = () =>{
 	//================================ III =========================================
 	const A = 0.4, B = 0.6;	//veroyzrnoct'
 	//load array arrGUR:
-	for (i = 0; i < size_matrix; i++) {
+	for (i = 0; i < l_y; i++) {
 		arrGUR[i] = (B * arrVA[i]) + (A * arrMA[i]);
 	}
 	//Search "kriteriya" metodth Gurvits
 	var gur1 = arrGUR[0];//var. for trash
-	for (i = 1; i < size_matrix; i++) {
+	for (i = 1; i < l_y; i++) {
 		if (gur1 < arrGUR[i]) {
 			gur1 = arrGUR[i];
 			gur = i;
@@ -164,7 +182,7 @@ bt_result.onclick = () =>{
 	//Search min. "riska"
 	var se1 = arrMA[0];	//var. for trash
 	//zicl:
-	for (i = 1; i < size_matrix; i++) {
+	for (i = 1; i < l_y; i++) {
 		if (se1 > arrMA[i]) {
 			se1 = arrMA[i];
 			se = i;
@@ -176,8 +194,8 @@ bt_result.onclick = () =>{
 	//================================== V =========================================
 	var AA = 0.33;	//"    ravnoveroyzrnost'        "
 	//zicl:
-	for (i = 0; i < size_matrix; i++) {
-		for (j = 0; j < size_matrix; j++) {
+	for (i = 0; i < l_y; i++) {
+		for (j = 0; j < l_x; j++) {
 			if (j == 0) {
 				arrLA[i] = arrSA[i][j] * AA;
 			}
@@ -188,7 +206,7 @@ bt_result.onclick = () =>{
 	}
 	//Searh "kriteriya" metodth Laplasa
 	var la1 = arrLA[0];//var. for trash
-	for (i = 1; i < size_matrix; i++) {
+	for (i = 1; i < l_y; i++) {
 		if (la1 < arrLA[i]) {
 			la1 = arrLA[i];
 			la = i;
@@ -198,10 +216,9 @@ bt_result.onclick = () =>{
 	res_td[4].innerHTML = "A" + (la + 1);//Output in V row.
 	//================================== END - V ===================================
 }
-////////////// END Part programm, which search number "strategii" ////////////////////////
-
+//////////////////////////////////////////////////////////////////////////////////////////
 ////////// Delete data in inputs and res-table ///////////////////////////////////////////
-var bt_reset = document.querySelector("div.reset");
+var bt_reset = document.querySelector("div#reset");
 bt_reset.onclick = () => {
 	var inputs_data = document.querySelectorAll("input");
 	for(i = 0; i < inputs_data.length; i++){
@@ -213,8 +230,7 @@ bt_reset.onclick = () => {
 		res_td[i].innerHTML = "";
 	}
 }
-////////// End Delete data in inputs and res-table ///////////////////////////////////////
-
+//////////////////////////////////////////////////////////////////////////////////////////
 ////////// Open info /////////////////////////////////////////////////////////////////////
 var bt_p	= document.querySelectorAll("div.bt_plus");		//все плюсики
 var h_text	= document.querySelectorAll(".hidden_text");	//все скрытые тексты
@@ -251,4 +267,4 @@ bt_p[1].onclick = () =>{
 		check[1]++;
 	}
 }
-////////// End Open info /////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
